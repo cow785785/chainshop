f2a455521551cea3f9ac56717f185a956963a018
cow785785 <zcbm030199@gmail.com>
2023-05-16 17:39:18 +0800

    0516member

--- src/components/RegisterView.vue
+++ src/components/RegisterView.vue
@@ -4,25 +4,70 @@ export default {
     return {
       account: "",
       password: "",
+      password2: "",
       username: "",
       birthday: "",
-      location: "",
-      tel: "",
+      address: "",
+      phone: "",
       isCheck: true,
     };
   },
   methods: {
     regis() {
-      //   if (this.isCheck) {
-      //     localStorage.setItem("account", this.account);
-      //   } else {
-      //     sessionStorage.setItem("account", this.account);
-      //   }
+      const body = {
+        useraccount: this.account,
+        password: this.password,
+        password2: this.password2,
+        username: this.username,
+        birthDate: this.birthday,
+        address: this.address,
+        phone: this.phone,
+      };
+      console.log(body);
+      fetch("http://localhost:8080/addMember", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          if (data.message != "註冊成功") {
+            alert("輸入有誤，或是有空格沒填寫");
+            return;
+          }
+          alert(
+            "會員註冊成功！\n" +
+              "會員資料\n會員 帳號 :" +
+              data.userAccount +
+              "\n會員名稱：" +
+              data.userName +
+              "\n會員密碼:" +
+              data.password +
+              "\n會員生日：" +
+              data.birthDate +
+              "\n會員地址：" +
+              data.address +
+              "\n會員電話：" +
+              data.phone +
+              "\n訊息 : " +
+              data.message,
+            this.$router.push("/loginView")
+          );
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("註冊失敗" + "\n" + error.message);
+        });
+
       localStorage.setItem("account", this.account);
-      this.$router.push("/loginView");
-      alert("註冊成功");
+      // this.$router.push("/loginView");
+      // alert("註冊成功");
     },
   },
+  mounted() {},
 };
 </script>
 <template>
@@ -36,26 +81,22 @@ export default {
       <input type="password" placeholder="輸入密碼" v-model="password" />
 
       <label for="password">確認密碼</label>
-      <input type="password" placeholder="確認密碼" v-model="password" />
+      <input type="password" placeholder="確認密碼" v-model="password2" />
 
       <label for="username">會員名稱</label>
       <input type="text" placeholder="會員名稱" v-model="username" />
 
-      <label for="birthday">出生日期</label>
-      <input
-        type="text"
-        placeholder="輸入出生年月日ex:1995/03/01"
-        v-model="birthday"
-      />
+      <label for="birthday">生日</label>
+      <input type="text" placeholder="ex:1995-03-01" v-model="birthday" />
 
       <label for="location">地址</label>
-      <input type="text" placeholder="輸入地址" v-model="location" />
+      <input type="text" placeholder="輸入地址" v-model="address" />
 
       <label for="tel">電話</label>
       <input
         type="text"
-        placeholder="輸入電話ex:0912345678或886912345678"
-        v-model="tel"
+        placeholder="ex:09-12345678或886開頭"
+        v-model="phone"
       />
     </div>
     <div class="keep">
