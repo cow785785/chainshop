98e00d394b5af02276f1b5a4cbaa05b654a9bcf0
DESKTOP-RRPRPE7\Lenovo <yz1410aa@gmail.com>
2023-05-25 15:29:09 +0800

    所有商品都能放入購物車了

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -3,11 +3,12 @@ import { RouterLink, RouterView } from "vue-router";
 
 export default {
    components: {},
-   props: ["title", "inventory", "price", "info", "image"],
+   props: ["title", "inventory", "price", "info", "code", "image"],
    data() {
       return {
          show: true,
          imgUrl: "../../public/img/hm1.jpg",
+         selectedQuantity: "1",
       };
    },
    methods: {
@@ -18,6 +19,76 @@ export default {
       changeShow() {
          this.$emit("switchCard");
       },
+      putIntoCart(code, selectedQuantity) {
+         const useraccount = localStorage.getItem("useraccount");
+         const productCode = code;
+         const quantity = parseInt(selectedQuantity);
+         //若沒有地址則透過API去取得
+         if (!this.address) {
+            fetch("http://localhost:8080/selectMember", {
+               method: "POST",
+               headers: {
+                  "Content-Type": "application/json",
+               },
+               body: JSON.stringify({
+                  useraccount: useraccount,
+               }),
+            })
+               .then((res) => res.json())
+               .then((data) => {
+                  this.address = data.address;
+                  this.addToCartWithAddress(
+                     this.address,
+                     productCode,
+                     quantity
+                  );
+               })
+               .catch((err) => {
+                  console.log(err);
+                  // 處理錯誤，例如顯示錯誤訊息或使用預設值
+                  this.addToCartWithAddress(
+                     "Default Address",
+                     productCode,
+                     quantity
+                  );
+               });
+         } else {
+            this.addToCartWithAddress(this.address, productCode, quantity);
+         }
+      },
+      addToCartWithAddress(address, productCode, quantity) {
+         const existingOrderIndex = this.orderList.findIndex(
+            (order) => order.productCode === productCode
+         );
+
+         if (existingOrderIndex !== -1) {
+            // 如果存在相同的 productCode 物件，將其 quantity 加總
+            this.orderList[existingOrderIndex].quantity += quantity;
+            this.orderList[existingOrderIndex].totalPrice =
+               this.price * this.orderList[existingOrderIndex].quantity;
+         } else {
+            const totalPrice = this.price * quantity;
+            const newOrder = {
+               useraccount: localStorage.getItem("useraccount"),
+               productCode: productCode,
+               quantity: quantity,
+               totalPrice: totalPrice,
+               deliveryAddress: address,
+               productsId: {
+                  productImg: this.image,
+                  productName: this.title,
+               },
+            };
+            this.orderList.push(newOrder);
+         }
+         sessionStorage.setItem("orderList", JSON.stringify(this.orderList));
+      },
+   },
+   mounted() {
+      const storedOrderList = sessionStorage.getItem("orderList");
+      if (storedOrderList) {
+         this.orderList = JSON.parse(storedOrderList);
+      }
    },
 };
 </script>
@@ -33,13 +104,21 @@ export default {
          <div class="buy-area">
             <div class="count-area">
                <!-- 商品數量用select抓 可以用資料庫中的數量當作極限 -->
-               <select name="quantity">
-                  <option v-for="product in inventory" value="product">
+               <select
+                  name="inventory"
+                  id="inventory"
+                  v-model="selectedQuantity"
+               >
+                  <option v-for="product in inventory">
                      {{ product }}
                   </option>
                </select>
             </div>
-            <button class="cart btn btn-sm btn-primary" type="button">
+            <button
+               class="cart btn btn-sm btn-primary"
+               @click="putIntoCart(code, selectedQuantity)"
+               type="button"
+            >
                カートに入れる
             </button>
          </div>
